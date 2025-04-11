442f30f9a155c20543caabb931f93109b33a0c72
vinushann <it22319760@my.sliit.lk>
2025-04-11 23:49:39 +0530

    uploading the backend part to remote

---
+++ src/main/java/com/skillshare/skillshare_platform/SkillSharePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.skillshare.skillshare_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSharePlatformApplication.class, args);
+	}
+
+}
