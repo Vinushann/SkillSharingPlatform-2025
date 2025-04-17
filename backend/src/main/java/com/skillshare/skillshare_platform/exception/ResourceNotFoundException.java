40b5f84eac8d8c9e5dc56f9a318a1ac5456402f7
Ayathma <ayathmaisri@gmail.com>
2025-04-18 04:18:09 +0530

    Created Post Model

--- backend/src/main/java/com/skillshare/skillshare_platform/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.skillshare.skillshare_platform.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
