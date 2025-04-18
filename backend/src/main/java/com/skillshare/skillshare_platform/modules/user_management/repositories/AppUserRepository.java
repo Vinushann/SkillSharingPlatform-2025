feb589ee9a7cff8eeb3044831a76aa481ad3070b
VishvaKavindya <105339444+FR0n1x@users.noreply.github.com>
2025-04-18 19:42:13 +0530

    Created User DTO,Repository and Service File

---
+++ backend/src/main/java/com/skillshare/skillshare_platform/modules/user_management/repositories/AppUserRepository.java
@@ -0,0 +1,21 @@
+package com.skillshare.skillshare_platform.modules.user_management.repositories;
+
+import com.skillshare.skillshare_platform.modules.user_management.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, String> {
+    Optional<AppUser> findByUsername(String username);
+    Optional<AppUser> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
+
+}
